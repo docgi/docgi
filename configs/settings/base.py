2d571f3ea9b62f12c4bf446f2ead495908a45ac8
nakhoa <nakhoa17@gmail.com>
2019-11-20 13:15:06 +0700

    update config for static file

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -177,6 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.path('static')
 
 
 # djangorestframework-simpple-jwt
